4630c15f6656bf5d57b093e6d76496ed9d3decab
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2024-06-12 10:22:12 +0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,21 @@ let position = 9
 let player = 1
 
 
+const generate_rand_number = function () {
+  return Math.floor(Math.random() * 6) + 1
+}
+
+const highlight_winner = function () {
+  if (player === 1) {
+    player_1_win.textContent = '⭐️ 🏆 ⭐️'
+    player_2_win.textContent = '🤬'
+  } else {
+    player_2_win.textContent = '⭐️ 🏆 ⭐️'
+    player_1_win.textContent = '🤬'
+  }
+}
+
+
 
 const move_figure = function (number) {
 
@@ -12,9 +27,29 @@ const move_figure = function (number) {
   const figure = document.createElement('div')
   figure.classList.add('figure')
 
-  document.getElementById(target_step).append(figure)
-
   position = target_step
+
+  if (position >= 2 && position <= 16) {
+
+    document.getElementById(position).append(figure)
+
+  } else if (position >= 17) {
+
+    position === 17
+    document.getElementById(17).append(figure)
+    btn_move_figure.disabled = true
+    highlight_winner()
+
+  } else if (position <= 1) {
+
+    position === 1
+    document.getElementById(1).append(figure)
+    btn_move_figure.disabled = true
+    highlight_winner()
+
+  }
+
+
 
 
 
@@ -27,7 +62,9 @@ btn_move_figure.addEventListener('click', function () {
 
   if (player === 1) {
 
-    const number = Number(input_step_number.value)
+    const number = generate_rand_number()
+
+    box_display_number.textContent = number
 
     move_figure(number)
 
@@ -35,7 +72,9 @@ btn_move_figure.addEventListener('click', function () {
 
   } else {
 
-    const number = -Number(input_step_number.value)
+    const number = -Number(generate_rand_number())
+
+    box_display_number.textContent = -number
 
     move_figure(number)
 
--- scripts/selectors.js
+++ scripts/selectors.js
@@ -1,3 +1,7 @@
-// Temporary container 
-const input_step_number = document.querySelector('.input')
+// Generate number of steps
+const box_display_number = document.querySelector('.display_number')
 const btn_move_figure = document.querySelector('.btn_move')
+
+// Winner boxes
+const player_1_win = document.getElementById('17')
+const player_2_win = document.getElementById('1')
